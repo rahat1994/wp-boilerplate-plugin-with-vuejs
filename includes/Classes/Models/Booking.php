f8d11eefdf8a9aae78ca7d6ba6ebe98c03f7fdcf
Rahat <rahat392@gmail.com>
2023-01-19 01:00:00 +0600

    renaming events and creating other models

--- includes/Classes/Models/Booking.php
+++ includes/Classes/Models/Booking.php
@@ -3,14 +3,14 @@
 namespace reventz\Classes\Models;
 use reventz\Classes\Models\BaseModel;
 defined( 'ABSPATH' ) || exit();
-class Event extends BaseModel
+class Booking extends BaseModel
 {
     /**
 	 * The table associated with the model.
 	 *
 	 * @var string
 	 */
-    protected $table = 're_events';
+    protected $table = 're_bookings';
 
 
 }
--- includes/Classes/Models/TicketType.php
+++ includes/Classes/Models/TicketType.php
@@ -3,14 +3,14 @@
 namespace reventz\Classes\Models;
 use reventz\Classes\Models\BaseModel;
 defined( 'ABSPATH' ) || exit();
-class Event extends BaseModel
+class TicketType extends BaseModel
 {
     /**
 	 * The table associated with the model.
 	 *
 	 * @var string
 	 */
-    protected $table = 're_events';
+    protected $table = 're_ticket_types';
 
 
 }
